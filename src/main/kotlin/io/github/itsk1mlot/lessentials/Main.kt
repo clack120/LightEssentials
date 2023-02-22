d443e5e7ec4114f37b97ba7e47daf5aaf24a71c0
Clack12 <115343194+clack120@users.noreply.github.com>
2023-02-22 18:55:26 +0900

    Update Main.kt

--- src/main/kotlin/io/github/itsk1mlot/lessentials/Main.kt
+++ src/main/kotlin/io/github/itsk1mlot/lessentials/Main.kt
@@ -1,14 +1,14 @@
 package io.github.itsk1mlot.lessentials
-
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main: JavaPlugin() {
-
-    override fun onEnable() {
-        println("Load!")
-    }
-
-    override fun onDisable() {
-        println("unload!")
-    }
+  
+  override fun onEnable() {
+    logger.info("Plugin is being enabled!")
+  }
+  
+  override fun onDisable() {
+    logger.info("Plugin is being disabled!")
+  }
+  
 }
